505b020829b160048a736c48288901fc45112921
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2025-11-12 16:25:51 -0300

    novo commit

--- aula.py
+++ aula.py
@@ -4,4 +4,13 @@
 
 print(123) #aparecer na tela do terminalclear
 
+#shift + alt pra cima - duplica a linha
+
+print("hoje é segunda-feiraclear")
+print("hoje é segunda-feiraclear")
+print("hoje é segunda-feiraclear")
+print("hoje é segunda-feiraclear")
+print("hoje é segunda-feiraclear")
+print("hoje é segunda-feiraclear")
+print("hoje é segunda-feiraclear")
 print("hoje é segunda-feiraclear")
