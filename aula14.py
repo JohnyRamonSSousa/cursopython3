885b2f577610cea2955df2c1a85c551a5c119cd7
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2025-11-24 16:28:36 -0300

    nova alteração

--- aula14.py
+++ aula14.py
@@ -1,9 +1,21 @@
-a = 'AAAAA'
-b = 'BBBBBB'
-c = 1.1
-string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
-formato = string.format(
-    nome1=a, nome2=b, nome3=c
+# a = 'AAAAA'
+# b = 'BBBBBB'
+# c = 1.1
+# string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+# formato = string.format(
+#     nome1=a, nome2=b, nome3=c
+# )
+
+# print(formato)
+
+
+nome1 = 'Johny'
+nome2 = 'Ramon'
+
+string = 'nome1={w} nome2={k}'
+
+formato1 = string.format(
+    w=nome1, k=nome2
 )
 
-print(formato)
+print(formato1)
